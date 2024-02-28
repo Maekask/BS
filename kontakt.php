88d422bfbae5e38510b8a7ffc471b3db9854fa46
Maekask <156500170+Maekask@users.noreply.github.com>
2024-02-28 14:29:50 +0200

    bs

--- kontakt.php
+++ kontakt.php
@@ -7,7 +7,20 @@
     <title>Teenused</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+<style>
+
+         .progress-wrapper {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 50px;
+        }
+        .oskusriba {
+            width: 50%;
+            margin-bottom: 20px;
+        }
+</style>
+    </head>
 
 <body>
     <div class="container">
@@ -51,51 +64,64 @@
         </nav>
     </div>
 
-    <div class="container mt-5">
-        <h1>Teenuse kalkulaator</h1>
-        <form action="" method="GET">
-            <div class="form-group">
-                <label for="toode1">HTML</label>
-                <input type="number" class="form-control" id="input1" name="HTML" required>
-            </div>
-            <div class="form-group">
-                <label for="toode2">CSS</label>
-                <input type="number" class="form-control" id="input2" name="CSS" required>
-            </div>
-            <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="noustun" name="noustun">
-          <label class="form-check-label" for="noustun">Nõustun teenusetingimustega</label>
-        </div>
-            <button type="submit" class="btn btn-primary">Arvuta</button>
-        </form>
+<div class="container">
+    <h1 class="text-center">Minu oskused</h1>
+    <div class="progress-wrapper">
+        <?php
 
+        $oskused = array("HTML", "CSS", "Bootstrap", "PHP");
+
+        $varvid = array("primary", "secondary", "success", "danger", "warning", "info", "dark");
+
+        foreach ($oskused as $index => $oskus) {
+            $riba = rand(10, 100);
+            $varviklass = $varvid[array_rand($varvid)];
+
+            echo '<div class="oskusriba">';
+            echo '<div class="progress">';
+            echo '<div class="progress-bar bg-' . $varviklass . '" role="progressbar" style="width: ' . $riba . '%" aria-valuenow="' . $riba . '" aria-valuemin="0" aria-valuemax="100">';
+            echo $oskus;
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+</div>
+
+<h1 class="text-center mt-5">Meie töötajad</h1>
+ <div style="display: flex;">
 <?php
-{
-    if (isset($_GET['HTML']) && isset($_GET['CSS']) && isset($_GET['noustun'])) {
-        $html = $_GET['HTML'];
-        $css = $_GET['CSS'];
-        
-        $kokku = $html + $css;
+$pildikaust = './img/';
 
-        echo '<div class="container mt-3">';
-        echo '<div class="alert alert-success" role="alert">';
-        echo 'HTML: ' . $html . ' koodi' . '<br>';
-        echo 'CSS: ' . $css . ' koodi'. '<br>';
-        echo 'Kokku: ' . $kokku . ' koodi';
-        echo '</div>';
-        echo '</div>';
-    } else {
-        echo '<div class="container mt-3">';
-        echo '<div class="alert alert-danger" role="alert">';
-        echo 'Palun täitke kõik väljad ja nõustuge teenusetingimustega.';
+$pildid = scandir($pildikaust);
+
+$pildid = array_diff($pildid, array('.', '..'));
+
+echo '<div class="row">';
+
+foreach ($pildid as $pilt) {
+    $imagePath = $pildikaust . $pilt;
+
+    if (is_file($imagePath) && in_array(strtolower(pathinfo($pilt, PATHINFO_EXTENSION)), array('jpg', 'jpeg', 'png', 'gif'))) {
+        $nimi = ucfirst(pathinfo($pilt, PATHINFO_FILENAME));
+
+        $epost = strtolower(pathinfo($pilt, PATHINFO_FILENAME)) . '@sinunimi.ee';
+
+        echo '<div class="col-md-2">';
+        echo '<div class="text-center">';
+        echo '<img src="' . $imagePath . '" alt="' . $nimi . '" class="img-fluid mb-2"><br>';
+        echo '<strong>' . $nimi . '</strong><br>';
+        echo '<em>' . $epost . '</em>';
         echo '</div>';
         echo '</div>';
     }
 }
+
+echo '</div>';
 ?>
 
-
-
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
